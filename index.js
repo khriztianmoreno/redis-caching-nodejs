b4729aa4068420854e10c149048f8be176d4a942
Khriztian Moreno <khriztianmoreno@gmail.com>
2021-10-19 20:43:34 -0500

    Redis callback

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const redis = require('redis');
 const responseTime = require('response-time');
 const { promisify } = require('util');
 
+// redis client
+const client = redis.createClient();
+
+// Create the express app
 const app = express();
 
 // Add response time header
@@ -11,17 +15,29 @@ app.use(responseTime());
 
 // Get all characters
 app.get('/character', async (req, res) => {
-  try {
-    // Fetching Data from Rick and Morty API
-    const response = await axios.get(
-      'https://rickandmortyapi.com/api/character'
-    );
+  // redis callbacks
 
-    // resond to client
-    res.json(response.data);
-  } catch (error) {
-    res.send(error.message);
-  }
+  // Get the characters from the cache
+  client.get('characters', async (err, reply) => {
+    if (reply) {
+      return res.json(JSON.parse(reply));
+    } else {
+      const response = await axios.get(
+        'https://rickandmortyapi.com/api/character'
+      );
+
+      client.set('characters', JSON.stringify(response.data.results), (err, reply) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(reply);
+
+        // resond to client
+        return res.json(response.data);
+      });
+    }
+  })
+
 });
 
 app.get('/character/:id', async (req, res) => {
